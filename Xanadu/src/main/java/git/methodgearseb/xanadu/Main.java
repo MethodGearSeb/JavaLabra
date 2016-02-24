365d40268e1021553daa4ee0b81e6e97d8ce6192
Sarviala Sebastian <sexsarvi@da5-cs-bk107-11.helsinki.fi>
2016-02-24 16:14:00 +0200

    GUI modifications

--- Xanadu/src/main/java/git/methodgearseb/xanadu/Main.java
+++ Xanadu/src/main/java/git/methodgearseb/xanadu/Main.java
@@ -7,6 +7,6 @@ public class Main {
     public static void main(String[] args) {
 //        LiittymaAutomatic liittyma = new LiittymaAutomatic();
         LiittymaManual liittyma = new LiittymaManual();
-        liittyma.setVisible(true);
+        liittyma.run();
     }
 }
--- Xanadu/src/main/java/git/methodgearseb/xanadu/liittyma/LiittymaManual.java
+++ Xanadu/src/main/java/git/methodgearseb/xanadu/liittyma/LiittymaManual.java
@@ -2,41 +2,44 @@ package git.methodgearseb.xanadu.liittyma;
 
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.colorchooser.AbstractColorChooserPanel;
 
 public class LiittymaManual extends javax.swing.JFrame {
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JPanel jPanel1;
+
+    private final JPanel a = new JPanel();
 
     public LiittymaManual() {
-        initComponents();
     }
 
-    private void initComponents() {
-        jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new JLabel();
-        jLabel2 = new JLabel();
-        GridLayout layout = new GridLayout(4, 4);
+    private void initComponents(final Container pane) {
+        GridLayout layout = new GridLayout(0, 4);
         BorderLayout layout1 = new BorderLayout();
         Dimension dimension = new Dimension(800, 800);
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMaximumSize(dimension);
         setMinimumSize(dimension);
 
-        jPanel1.setLayout(layout);
-        jPanel1.setSize(dimension);
+        a.setLayout(layout);
+        a.setSize(dimension);
 
-        jLabel1.setText("jLabel1");
-        jPanel1.add(jLabel1);
-
-        jLabel2.setText("jLabel2");
-        jPanel1.add(jLabel2);
+        for (int i = 0; i < 16; i++) {
+            JPanel p = new JPanel();
+            JPanel a = new JPanel();
+            JPanel b = new JPanel();
+            p.setLayout(new GridLayout(0, 2));
+            a.setBackground(Color.red);
+            b.setBackground(Color.blue);
+            p.add(a);
+            p.add(b);
+            pane.add(p);
+        }
     }
-    
-    private void run() {
-        getContentPane().add(jPanel1, "Center");
-        pack();
-        
+
+    public void run() {
+        LiittymaManual frame = new LiittymaManual();
+        frame.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        frame.initComponents(frame.getContentPane());
+        frame.pack();
+        frame.setVisible(true);
     }
 }
